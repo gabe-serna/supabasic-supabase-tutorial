dff9f5c846ade9d92d13d1857cbe13bdf8df080e
Gabe Serna <gabriel.serna118@gmail.com>
2024-10-09 07:38:33 -0400

    Add All Things Column

--- app.js
+++ app.js
@@ -30,14 +30,37 @@ const subjectInput = document.getElementById("subjectInput");
 
 //event listeners
 loginButton.addEventListener("click", () => {
-  console.log("clicked");
   supaClient.auth.signInWithOAuth({
     provider: "google"
   });
 });
 
+createThing.addEventListener("click", async () => {
+  const {
+    data: { user }
+  } = await supaClient.auth.getUser();
+  const thing = createRandomThing(user);
+  await supaClient.from("things").insert([thing]);
+});
+
+logoutButton.addEventListener("click", () => {
+  supaClient.auth.signOut();
+});
+
 //init
 checkUserOnStartUp();
+let myThingsSubscription;
+const myThings = {};
+const allThings = {};
+getAllInitialThings().then(() => listenToAllThings());
+
+supaClient.auth.onAuthStateChange((_event, session) => {
+  if (session?.user) {
+    adjustForUser(session.user);
+  } else {
+    adjustForNoUser();
+  }
+});
 
 // function declarations
 async function checkUserOnStartUp() {
@@ -57,7 +80,7 @@ function adjustForUser(user) {
   myThingsSection.hidden = false;
   userDetails.innerHTML = `
   <h3>Welcome, ${user.user_metadata.full_name}</h3>
-  <img src="${user.user_metadata.avatar_url}" alt='profile picture' title='profile picture' />
+  <img src="${user.user_metadata.avatar_url}" />
   <p>UID: ${user.id}</p>
   `;
 }
@@ -67,5 +90,84 @@ function adjustForNoUser() {
   whenSignedOut.hidden = false;
   myThingsSection.hidden = true;
 }
+
+async function getAllInitialThings() {
+  const { data } = await supaClient.from("things").select();
+  for (const thing of data) {
+    allThings[thing.id] = thing;
+  }
+  renderAllThings();
+}
+
+function renderAllThings() {
+  const tableHeader = `
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Weight</th>
+    </tr>
+  </thead>
+  `;
+  const tableBody = Object.values(allThings)
+    .sort((a, b) => (a.weight > b.weight ? -1 : 1))
+    .map(thing => {
+      return `
+      <tr>
+        <th>${thing.name}</th>
+        <th>${thing.weight} lbs</th>
+      </tr>`;
+    })
+    .join("");
+  const table = `
+    <table class="table table-striped">
+      ${tableHeader}
+      <tbody>${tableBody}</tbody>
+    </table>`;
+  allThingsList.innerHTML = table;
+}
+
+function createRandomThing(user) {
+  if (!user) {
+    console.error("Must be signed in to create a thing!");
+    return;
+  }
+  return {
+    name: faker.commerce.productName(3),
+    weight: Math.round(Math.random() * 100),
+    owner: user.id
+  };
+}
+
+function handleAllThingsUpdate(update) {
+  if (update.eventType === "DELETE") {
+    delete allThings[update.old.id];
+  } else {
+    allThings[update.new.id] = update.new;
+  }
+  renderAllThings();
+}
+
+function listenToAllThings() {
+  supaClient
+    .channel(`public:things`)
+    .on(
+      "postgres_changes",
+      { event: "*", schema: "public", table: "things" },
+      handleAllThingsUpdate
+    )
+    .subscribe();
+}
+
+async function getAllInitialThings(user) {}
+function handleMyThingsUpdate(update) {}
+
+async function listenToMyThingsChanges(user) {}
+function renderMyThings() {}
+
+function deleteButtonTemplate(thing) {}
+
+async function deleteAtId(id) {}
+
+const trashIcon = ``;
 
 //npx nodemon -e "js,html" -x "npx http-server --port 3000"
